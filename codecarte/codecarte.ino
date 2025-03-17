fd607a156e2b5ff11baf30793a02b2296e8dee58
bigmamalge <82677497+bigmamalge@users.noreply.github.com>
2025-03-17 16:22:08 +0100

    Add files via upload

--- codecarte/codecarte.ino
+++ codecarte/codecarte.ino
@@ -1,25 +1,47 @@
+
+#define EN 8   
+#define Y_DIR 6   
+#define Y_STEP 3 
+#define Z_DIR 7  
+#define Z_STEP 4
+#define A_DIR 13 
+
+const int pulseWidthMicros = 500;
+
 #include <Wire.h>
 #include "rgb_lcd.h"
 
 rgb_lcd lcd;
 
-const int LEDPIN = 13;
 String CommandString = "";
 String InputData = "";
 bool IS_Data_Complete = false;
 char carac;
 unsigned char inchar;
+unsigned char test;
 int taille;
 int emplacement;
+int espacementpoint = 200;
+int espacementcarac = 400;
+
 
 
 
 void setup() {
-  pinMode(LEDPIN, OUTPUT);
   Serial.begin(9600);
+  pinMode(22, OUTPUT);
 
   lcd.begin(16, 2);
   lcd.print("impression :");
+
+  pinMode(EN, OUTPUT);
+  digitalWrite(EN, LOW);
+
+  pinMode(Y_STEP, OUTPUT);
+  pinMode(Y_DIR, OUTPUT);
+  pinMode(Z_STEP, OUTPUT);
+  pinMode(Z_DIR, OUTPUT);
+
 
 
 }
@@ -38,88 +60,113 @@ void Serial_Data_Events()
     Serial.flush();
 
     inchar = (unsigned char)Serial.read();
+    test = inchar;
 
   lcd.setCursor(0, 1);
   lcd.print("                ");
 
-  int car8 = inchar%2;
-  int car7 = inchar%4 >> 1;
-  int car6 = inchar%8 >> 2;
-  int car5 = inchar%16 >> 3;
-  int car4 = inchar%32 >> 4;
-  int car3 = inchar%64 >> 5;
+  int car8 = test%2;
+  int car7 = test%4 >> 1;
+  int car6 = test%8 >> 2;
+  int car5 = test%16 >> 3;
+  int car4 = test%32 >> 4;
+  int car3 = test%64 >> 5;
 
-  unsigned char cartout = inchar;
-  unsigned char car1et2 = inchar / 64;
+  unsigned char cartout = test;
+  unsigned char car1et2 = test / 64;
   
-
+ 
     switch (car1et2)
     {
       case 0b00:
-        
         if (car3 == 1)
           {
             lcd.setCursor(0, 1);
             lcd.print("1");
+            electro();
+            MoteurY(100,true);
           }
         else
-          {
+          { 
             lcd.setCursor(0, 1);
             lcd.print("0");
+            MoteurY(100,true);
           }
         
         if (car4 == 1)
           {
             lcd.print("1");
+            electro();
+            MoteurY(100,true);
+            MoteurX(100,true);
           }
         else
           {
             lcd.print("0");
+            MoteurY(100,true);
+            MoteurX(100,true);
           }
           
         if (car5 == 1)
           {
             lcd.print("1");
+            electro();
+            MoteurY(100,true);
           }
         else
           {
             lcd.print("0");
+            MoteurY(100,true);
           }
           
         if (car6 == 1)
           {
             lcd.print("1");
+            electro();
+            MoteurY(100,true);
+            MoteurX(100,true);
           }
         else
           {
             lcd.print("0");
+            MoteurY(100,true);
+            MoteurX(100,true);
           }
           
         if (car7 == 1)
           {
             lcd.print("1");
+            electro();
+            MoteurY(100,true);
           }
         else
           {
             lcd.print("0");
+            MoteurY(100,true);
           }
 
         if (car8 == 1)
           {
             lcd.print("1");
+            electro();
+            MoteurY(200,true);
+            MoteurX(300,true);
           }
         else
           {
             lcd.print("0");
+            MoteurY(200,true);
+            MoteurX(300,true);
           }
         break;
-
+      
       case 0b10:
         switch (cartout)
         {
           case 0b10100010 :
             lcd.setCursor(0, 1);
             lcd.print("fini  ");
+            Serial.println("fin");
             break;
           
           case 0b10100100 :
@@ -140,11 +187,57 @@ void Serial_Data_Events()
 
     }
 
-    delay(1000);
+    delay(300);
   }
 }
 
 
+void MoteurY(int step,bool  sens)
+{
+  if(sens)
+  {
+    digitalWrite(Y_DIR, HIGH);
+  }
+  else
+  {
+    digitalWrite(Y_DIR, LOW);
+  }
+
+  for (int i = 0;i <= step; i++)
+  {
+    digitalWrite(Y_STEP, HIGH);
+    delay(1);
+    digitalWrite(Y_STEP, LOW);
+    delay(1);
+  }
+  
+}
+
+void MoteurX(int step,bool  sens)
+{
+  if(sens)
+  {
+    digitalWrite(Z_DIR, HIGH);
+  } 
+  else
+  {
+    digitalWrite(Z_DIR, LOW);
+  }
+  for (int i = 0;i <= step; i++)
+  {
+    digitalWrite(Z_STEP, HIGH);
+    delay(1);
+    digitalWrite(Z_STEP, LOW);
+    delay(1);
+  }
+  
+}
+void electro()
+{
+  digitalWrite(22, HIGH);
+  delay(20);
+  digitalWrite(22, LOW);
+}
 
 
 
